4eb27d09674262635806e3be30d9f9d1df4e55b2
AndreLPDZ <lpdzandre@gmail.com>
2025-02-26 11:55:55 -0300

    Create cookies-com-php.php

---
+++ youtube/cookies-com-php.php
@@ -0,0 +1,18 @@
+<?php
+setcookie("produto","celular", time() + (7 * 24 * 3600)); // 86400 = 1 day  
+?>
+
+<!--
+Aprenda de forma simples e prática como utilizar cookies no PHP com a função setcookie().
+Neste vídeo, explico como criar, recuperar e excluir cookies para armazenar informações
+temporárias no navegador do usuário. Ideal para quem deseja entender o funcionamento
+dessa funcionalidade essencial no desenvolvimento web.
+
+📌 Código-fonte disponível no GitHub!
+
+📌 Assista agora e facilite a gestão de sessões no seu projeto PHP.
+
+🔗 Assista no Youtube: [https://www.youtube.com/watch?v=K0tzT70qP3g]
+
+Se esse conteúdo te ajudou, siga nosso git para mais tutoriais! 🚀
+-->
