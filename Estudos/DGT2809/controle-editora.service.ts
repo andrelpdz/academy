3bc73bdd16dda67dcc3c6aed6d39861dfe3a616a
AndreLPDZ <lpdzandre@gmail.com>
2025-11-15 14:58:31 -0300

    Create controle-editora.service.ts

---
+++ Estudos/DGT2809/controle-editora.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Editora } from './editora';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ControleEditoraService {
+  private editoras: Array<Editora> = [
+    new Editora(1, 'Alta Books'),
+    new Editora(2, 'Pearson Education do Brasil'),
+    new Editora(3, 'Addison Wesley')
+  ];
+
+  constructor() {}
+
+  getEditoras(): Array<Editora> {
+    return this.editoras;
+  }
+
+  getNomeEditora(codEditora: number): string {
+    const editora = this.editoras.filter(e => e.codEditora === codEditora);
+    return editora.length > 0 ? editora[0].nome : '';
+  }
+}
